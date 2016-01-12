513630673dff5476f43523825202233c69d57659
Kevin Sullivan <kevin@bitantics.com>
2016-01-11 22:43:20 -0800

    Change over time

--- src/Main.purs
+++ src/Main.purs
@@ -1,13 +1,22 @@
 module Main where
 
 import Prelude
+import Math ((%))
+
 import Control.Monad.Eff (Eff)
+import Control.Timer (Timer())
+
 import Data.Maybe (Maybe(..))
 import Data.Int (toNumber)
 import Data.Array ((..), concat)
+
+import Signal ((~>), Signal(), runSignal)
+import Signal.DOM (animationFrame)
+
+import DOM (DOM())
 import Graphics.Canvas (
   Canvas(), Context2D(), ImageData(),
-  getCanvasElementById, getContext2D, putImageData
+  getCanvasElementById, getContext2D, putImageData, clearRect
 )
 
 width :: Int
@@ -37,24 +46,36 @@ noiseGrid w h = noiseAt <$> grid w h
 -- image --
 foreign import createImageData :: Array Int -> Int -> Int -> ImageData
 
-noisePixel :: Number -> Array Int
-noisePixel n = [ np 190, np 186, np 209, np 255 ]
+noisePixel :: Number -> Number -> Array Int
+noisePixel t n = [ np 190, np 186, np 209, np 255 ]
   where
-    np max | 0.49 < n && n < 0.51 = max
+    offset = t % 1.0
+    np max | (-0.01 + offset) < n && n < (0.01 + offset) = max
            | otherwise            = 0
 
-image :: ImageData
-image = createImageData imageData width height
-  where imageData = concat $ noisePixel <$> noiseGrid width height
+image :: Number -> ImageData
+image t = createImageData imageData width height
+  where imageData = concat $ noisePixel t <$> noiseGrid width height
 
-renderImage :: forall eff. Context2D -> Eff (canvas :: Canvas | eff) Unit
-renderImage ctx = do
-  putImageData ctx image 0.0 0.0
+renderImage :: forall eff. Context2D -> Number -> Eff (canvas :: Canvas | eff) Unit
+renderImage ctx t = do
+  clearRect ctx {
+    x: 0.0, y: 0.0,
+    w: toNumber width, h: toNumber height
+  }
+  putImageData ctx (image t) 0.0 0.0
   return unit
 
+seconds :: forall eff. Eff (dom :: DOM, timer :: Timer | eff) (Signal Number)
+seconds = do
+  nowMs <- animationFrame
+  return $ nowMs ~> secs
+    where secs ms = ms / 1000.0
+
 -- main --
-main :: forall eff. Eff (canvas :: Canvas | eff) Unit
+main :: forall eff. Eff (timer :: Timer, dom :: DOM, canvas :: Canvas | eff) Unit
 main = do
   Just canvas <- getCanvasElementById "topological"
   ctx <- getContext2D canvas
-  renderImage ctx
+  s <- seconds
+  runSignal $ s ~> (renderImage ctx)
