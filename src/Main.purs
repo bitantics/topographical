0232fd8a0b55f58bc46bd9d58599d8a3ff3ceca7
Kevin Sullivan <kevin@bitantics.com>
2016-01-15 22:43:00 -0800

    Much better looking static image

--- src/Main.purs
+++ src/Main.purs
@@ -1,17 +1,13 @@
 module Main where
 
 import Prelude
-import Math ((%))
+import Math (pow, sqrt, pi, exp)
 
 import Control.Monad.Eff (Eff)
-import Control.Timer (Timer())
 
 import Data.Maybe (Maybe(..))
-import Data.Int (toNumber)
+import Data.Int (toNumber, round)
 import Data.Array ((..), concat)
-
-import Signal ((~>), Signal(), runSignal)
-import Signal.DOM (animationFrame)
 
 import DOM (DOM())
 import Graphics.Canvas (
@@ -20,10 +16,10 @@ import Graphics.Canvas (
 )
 
 width :: Int
-width = 400
+width = 1600
 
 height :: Int
-height = 225
+height = 900
 
 -- coordinates --
 newtype Coord = Coord { x :: Int, y :: Int }
@@ -46,36 +42,34 @@ noiseGrid w h = noiseAt <$> grid w h
 -- image --
 foreign import createImageData :: Array Int -> Int -> Int -> ImageData
 
-noisePixel :: Number -> Number -> Array Int
-noisePixel t n = [ np 190, np 186, np 209, np 255 ]
+gaussian :: Number -> Number -> Number
+gaussian mean n = coefficient * (exp exponent)
+  where 
+    dev = 0.4
+    coefficient = 1.0 / (dev * (sqrt (2.0 * pi)))
+    exponent = (-3000.0 * (pow (n - mean) 2.0)) / (2.0 * (pow dev 2.0))
+
+noisePixel :: Number -> Array Int
+noisePixel n = [ 190, 186, 209, round (opacity * 255.0) ]
   where
-    offset = t % 1.0
-    np max | (-0.01 + offset) < n && n < (0.01 + offset) = max
-           | otherwise            = 0
+    opacity = gaussian 0.1 n + gaussian 0.3 n + gaussian 0.5 n + gaussian 0.7 n + gaussian 0.9 n
 
-image :: Number -> ImageData
-image t = createImageData imageData width height
-  where imageData = concat $ noisePixel t <$> noiseGrid width height
+image :: ImageData
+image = createImageData imageData width height
+  where imageData = concat $ noisePixel <$> noiseGrid width height
 
-renderImage :: forall eff. Context2D -> Number -> Eff (canvas :: Canvas | eff) Unit
-renderImage ctx t = do
+renderImage :: forall eff. Context2D -> Eff (canvas :: Canvas | eff) Unit
+renderImage ctx = do
   clearRect ctx {
     x: 0.0, y: 0.0,
     w: toNumber width, h: toNumber height
   }
-  putImageData ctx (image t) 0.0 0.0
+  putImageData ctx image 0.0 0.0
   return unit
 
-pulse :: forall eff. Eff (dom :: DOM, timer :: Timer | eff) (Signal Number)
-pulse = do
-  nowMs <- animationFrame
-  return $ nowMs ~> secs
-    where secs ms = ms / 30000.0
-
 -- main --
-main :: forall eff. Eff (timer :: Timer, dom :: DOM, canvas :: Canvas | eff) Unit
+main :: forall eff. Eff (dom :: DOM, canvas :: Canvas | eff) Unit
 main = do
   Just canvas <- getCanvasElementById "topological"
   ctx <- getContext2D canvas
-  p <- pulse
-  runSignal $ p ~> (renderImage ctx)
+  renderImage ctx
